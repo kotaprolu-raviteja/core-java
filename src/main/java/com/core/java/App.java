0ac940a2a601d6438306a20e0d4bfd460e3a86b9
kotaproluraviteja3012 <kotaprolu.raviteja@gmail.com>
2019-07-28 21:38:00 -0400

    initial commit

---
+++ src/main/java/com/core/java/App.java
@@ -0,0 +1,13 @@
+package com.core.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
